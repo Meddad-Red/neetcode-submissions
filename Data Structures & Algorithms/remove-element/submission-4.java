2f31486b49c3f688687bc7bca4b23c395bfbc1b9
Meddad, Mohamed Redha <125776575+Meddad-Red@users.noreply.github.com>
2026-05-05 00:03:51 -0400

    Add: remove-element - submission-4

---
+++ Data Structures & Algorithms/remove-element/submission-4.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {                       
+        int k=0;
+        if(nums.length==1 && nums[0]==val){
+            return k;
+        }           
+        
+        int j=0;
+
+        for(int i=0; i<nums.length; i++){           
+            if(nums[i]!=val){  
+                nums[k]=nums[i];
+                k++;             
+            }     
+           
+        }
+        return k;
+    }
+}
