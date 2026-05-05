1d1ae0e65968099249dd2a12d1f7931fe958a978
Meddad, Mohamed Redha <125776575+Meddad-Red@users.noreply.github.com>
2026-05-04 23:22:00 -0400

    Add: longest-common-prefix - submission-7

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-7.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        // simple vertical scanning
+        if(strs==null||strs.length==0){
+            return "";
+        }
+        if(strs.length==1){
+            return strs[0];
+        }   
+
+        String firstStr = strs[0];
+
+        for(int i=0; i<firstStr.length(); i++){
+            char charFirstStr = firstStr.charAt(i);
+
+            for(int j=1; j<strs.length; j++){
+                if(i >= strs[j].length() || strs[j].charAt(i) != charFirstStr){
+                    return firstStr.substring(0,i);
+                }
+            }
+        }
+        return firstStr;
+    }
+}
