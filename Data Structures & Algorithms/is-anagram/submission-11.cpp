825695ed74c088cd51c4a895ea3303db2d8004eb
Meddad, Mohamed Redha <125776575+Meddad-Red@users.noreply.github.com>
2026-05-08 18:46:03 -0400

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+        array<int,26> count ={};
+
+        for(int i=0; i<s.length();i++){
+            count[s[i]-'a']++;
+            count[t[i]-'a']--;
+        }
+
+        for(int freq:count){
+            if(freq!=0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
