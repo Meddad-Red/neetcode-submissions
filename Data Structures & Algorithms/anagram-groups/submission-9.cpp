711398f9786223595639d9fb8a17f76e155fa7a1
Meddad, Mohamed Redha <125776575+Meddad-Red@users.noreply.github.com>
2026-05-09 00:28:37 -0400

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> groups;
+
+        for (string word: strs){
+            array<int,26> count = {};
+
+            for(char c: word){
+                count[c-'a']++;
+            }
+
+            string key = "";
+
+            for(int freq:count){
+                key += '#' + to_string(freq);
+            }
+
+            groups[key].push_back(word);            
+        }
+
+        vector<vector<string>> result;
+
+        for(auto pair:groups){
+            result.push_back(pair.second);
+        }
+
+        return result;
+    }
+};
