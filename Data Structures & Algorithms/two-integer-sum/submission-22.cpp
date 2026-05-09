6ad0b117966ad510f7229517d57c7f6923d2a317
Meddad, Mohamed Redha <125776575+Meddad-Red@users.noreply.github.com>
2026-05-08 22:52:51 -0400

    Add: two-integer-sum - submission-22

---
+++ Data Structures & Algorithms/two-integer-sum/submission-22.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       std::unordered_map<int,int> indexSum;
+
+       for(int i=0; i<nums.size();i++){
+        int complement = target - nums[i];
+
+        if(indexSum.count(complement)){
+            return {indexSum[complement],i};
+        }
+
+
+        indexSum[nums[i]]=i;
+       }
+    }
+};
