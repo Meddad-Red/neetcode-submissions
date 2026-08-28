38271d0786d812bf66fb29b5e335354cba44bfd4
Meddad, Mohamed Redha <125776575+Meddad-Red@users.noreply.github.com>
2026-08-28 00:11:52 -0400

    Add: products-of-array-discluding-self - submission-9

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-9.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {    
+        int[] prodL = new int[nums.length];
+        Arrays.fill(prodL,1);
+
+        for(int left=0; left<nums.length-1;left++){
+            prodL[left+1]=nums[left]*prodL[left];
+        }
+
+        int[] prodR = new int[nums.length];
+        Arrays.fill(prodR,1);
+        
+      
+        for(int right=nums.length-1; right>0; right--){
+            prodR[right-1]=nums[right]*prodR[right];
+        }
+
+        for(int i=0;i<nums.length;i++){
+            prodR[i]*=prodL[i];
+        }
+        return prodR;
+
+
+    }
+}  
